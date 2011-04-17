4f70ded5177068e312e616bcc4ec7c5855210766
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2011-04-17 18:20:14 +0200

    Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -19,11 +19,15 @@ ActiveRecord::Schema.define(:version => 20110409125343) do
     t.datetime "updated_at"
   end
 
+  add_index "media", ["medium_category_id"], :name => "index_media_on_medium_category_id"
+
   create_table "medium_categories", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "medium_categories", ["name"], :name => "index_medium_categories_on_name"
 
   create_table "place_categories", :force => true do |t|
     t.string   "name"
@@ -31,12 +35,14 @@ ActiveRecord::Schema.define(:version => 20110409125343) do
     t.datetime "updated_at"
   end
 
-  create_table "place_category_places", :force => true do |t|
-    t.integer  "place_id"
-    t.integer  "place_category_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  add_index "place_categories", ["name"], :name => "index_place_categories_on_name"
+
+  create_table "place_categories_places", :id => false, :force => true do |t|
+    t.integer "place_id"
+    t.integer "place_category_id"
   end
+
+  add_index "place_categories_places", ["place_category_id", "place_id"], :name => "index_place_categories_places_on_place_category_id_and_place_id"
 
   create_table "places", :force => true do |t|
     t.string   "name"
@@ -47,6 +53,8 @@ ActiveRecord::Schema.define(:version => 20110409125343) do
     t.datetime "updated_at"
   end
 
+  add_index "places", ["point_id"], :name => "index_places_on_point_id"
+
   create_table "points", :force => true do |t|
     t.decimal  "x",          :precision => 10, :scale => 0
     t.decimal  "y",          :precision => 10, :scale => 0
@@ -54,6 +62,8 @@ ActiveRecord::Schema.define(:version => 20110409125343) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "points", ["user_id"], :name => "index_points_on_user_id"
 
   create_table "traceroute_points", :force => true do |t|
     t.integer  "traceroute_id"
@@ -64,12 +74,7 @@ ActiveRecord::Schema.define(:version => 20110409125343) do
     t.datetime "updated_at"
   end
 
-  create_table "traceroute_users", :force => true do |t|
-    t.integer  "traceroute_id"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+  add_index "traceroute_points", ["traceroute_id", "point_id"], :name => "index_traceroute_points_on_traceroute_id_and_point_id"
 
   create_table "traceroutes", :force => true do |t|
     t.string   "name"
@@ -79,13 +84,25 @@ ActiveRecord::Schema.define(:version => 20110409125343) do
     t.datetime "updated_at"
   end
 
+  add_index "traceroutes", ["user_id"], :name => "index_traceroutes_on_user_id"
+
+  create_table "traceroutes_users", :id => false, :force => true do |t|
+    t.integer "traceroute_id"
+    t.integer "user_id"
+  end
+
+  add_index "traceroutes_users", ["traceroute_id", "user_id"], :name => "index_traceroutes_users_on_traceroute_id_and_user_id"
+
   create_table "users", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
 
 end
