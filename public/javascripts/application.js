99655b1a2041a1148b1c6723bd3ff108c096ca57
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2011-05-15 13:47:19 +0200

    Smaller map for place

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -31,26 +31,81 @@ function placeMarker(map, location) {
 	return marker;
 }
 
-function init() {
-        var mapDiv = document.getElementById('map');
-        var krakow = new google.maps.LatLng(50.061933, 19.937611);
-        var options = {
-            center: krakow,
-            zoom: 10,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            mapTypeControl: true
-        };
-        var map = new google.maps.Map(mapDiv, options);
-
-        google.maps.event.addListener(map, 'click', function(event) {
-            point = event.latLng;
-            x = point.toString().replace("(","").split(", ")[0];
-            y = point.toString().replace(")","").split(", ")[1];
-            placeMarker(map, point);
-            addPoint(x, y);
-        });
-
+function setMapOptions(map){
+	// Coordinates
+	lat = document.getElementById('latitude');
+	lng = document.getElementById('longitude');
+	if(lat != null && lng != null){
+		lat = lat.value;
+		lng = lng.value;
+		position = new google.maps.LatLng(lat, lng);
+		map.setOptions({center: position});
+		var marker = new google.maps.Marker({
+			position: position,
+			map: map
+		});
+		// Draggable marker
+		draggable = document.getElementById('draggable');
+		if(draggable != null){
+			marker.setDraggable(true);
+			// Set listener on dragend
+			google.maps.event.addListener(marker, 'dragend', function(event) {
+				position = marker.getPosition();
+				y = position.toString().replace("(","").split(", ")[0];
+				x = position.toString().replace(")","").split(", ")[1];
+				document.getElementById('place_point_y').value = y;
+				document.getElementById('place_point_x').value = x;
+			});
+		}
+	}
+	// Zoom
+	zoom = document.getElementById('zoom');
+	if(zoom != null){
+		map.setOptions({zoom: parseInt(zoom.value)});
+	}
+	// Disable Default UI
+	disableDefaultUI = document.getElementById('disableDefaultUI');
+	if(disableDefaultUI != null){
+		map.setOptions({disableDefaultUI: true});
+	}
+	// Add map listener for new place
+	addNewPlace = document.getElementById('addNewPlace');
+	if(addNewPlace != null){
+		google.maps.event.addListener(map, 'click', function(event) {
+			position = event.latLng;
+			// Limit to only 1 marker
+			if(markers.length == 0){
+				marker = placeMarker(map, position);
+				y = position.toString().replace("(","").split(", ")[0];
+				x = position.toString().replace(")","").split(", ")[1];
+				document.getElementById('place_point_y').value = y;
+				document.getElementById('place_point_x').value = x;
+				
+				// Set listener on dragend
+				google.maps.event.addListener(marker, 'dragend', function(event) {
+					position = marker.getPosition();
+					y = position.toString().replace("(","").split(", ")[0];
+					x = position.toString().replace(")","").split(", ")[1];
+					document.getElementById('place_point_y').value = y;
+					document.getElementById('place_point_x').value = x;
+				});
+			}
+		});
     }
+}
+
+function init() {
+	var mapDiv = document.getElementById('map');
+	var options = {
+		center: new google.maps.LatLng(50.061933, 19.937611),
+		zoom: 10,
+		disableDefaultUI: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		mapTypeControl: true
+	};
+	var map = new google.maps.Map(mapDiv, options);
+	setMapOptions(map);
+}
 
 window.onload = init;
 
